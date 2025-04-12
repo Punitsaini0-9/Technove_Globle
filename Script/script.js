8a342d839544422e7f41695958fac2e36c3fb37f
Your Name <gsingh3_be21@thapar.edu>
2025-04-12 17:14:29 +0530

    changes

--- Script/script.js
+++ Script/script.js
@@ -3,29 +3,29 @@
     {
       text: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
       name: "Client 1",
-      img: "https://i.pravatar.cc/100?img=5"
+      img: "https://i.pravatar.cc/100?img=5",
     },
     {
       text: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
       name: "Client 2",
-      img: "https://i.pravatar.cc/100?img=12"
+      img: "https://i.pravatar.cc/100?img=12",
     },
     {
       text: "An unknown printer took a galley of type and scrambled it to make a type specimen book.",
       name: "Client 3",
-      img: "https://i.pravatar.cc/100?img=36"
+      img: "https://i.pravatar.cc/100?img=36",
     },
   ];
-  
+
   // ✅ Only declare this ONCE
   let current = 0;
-  
+
   function updateTestimonial() {
     const t = testimonials[current];
     const img = document.getElementById("clientImg");
     const text = document.getElementById("testimonialText");
     const name = document.getElementById("clientName");
-  
+
     if (img && text && name) {
       img.src = t.img;
       img.onerror = function () {
@@ -35,22 +35,24 @@
       name.innerText = t.name;
     }
   }
-  
+
   function prevTestimonial() {
     current = (current - 1 + testimonials.length) % testimonials.length;
     updateTestimonial();
   }
-  
+
   function nextTestimonial() {
     current = (current + 1) % testimonials.length;
     updateTestimonial();
   }
-  
+
   document.addEventListener("DOMContentLoaded", updateTestimonial);
-  
-  // ✅ Auto-cycle
+
   setInterval(() => {
     nextTestimonial();
   }, 5000);
-  
+
+  // 👇 Make functions available globally
+  window.prevTestimonial = prevTestimonial;
+  window.nextTestimonial = nextTestimonial;
 })();
