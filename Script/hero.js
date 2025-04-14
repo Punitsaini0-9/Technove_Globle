b79340e78de63d2b65e5e44ab88620b1bdb8d179
Your Name <gsingh3_be21@thapar.edu>
2025-04-14 17:16:14 +0530

    changes

--- Script/hero.js
+++ Script/hero.js
@@ -9,3 +9,18 @@ setInterval(() => {
 
 inputs[current].checked = true;
 inputs[current].focus();
+
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    const hero = document.querySelector("#home");
+
+    // Get the bottom position of the hero section
+    const heroBottom = hero.offsetTop + hero.offsetHeight;
+
+    // If scrollY goes past the hero's bottom, add 'scrolled' class
+    if (window.scrollY >= heroBottom) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
--- Script/script.js
+++ Script/script.js
@@ -56,3 +56,15 @@
   window.prevTestimonial = prevTestimonial;
   window.nextTestimonial = nextTestimonial;
 })();
+
+
+  window.addEventListener("scroll", function () {
+      const navbar = document.querySelector(".navbar");
+      if (window.scrollY > 50) {
+          navbar.classList.add("scrolled");
+      } else {
+          navbar.classList.remove("scrolled");
+      }
+  });
+
+
