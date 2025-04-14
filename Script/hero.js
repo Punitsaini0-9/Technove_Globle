551c97d182b1261047a3fc018b823ac84648f36d
Your Name <gsingh3_be21@thapar.edu>
2025-04-14 17:22:57 +0530

    changes

--- Script/hero.js
+++ Script/hero.js
@@ -10,14 +10,7 @@ setInterval(() => {
 inputs[current].checked = true;
 inputs[current].focus();
 
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
+
 
 window.addEventListener("scroll", function () {
     const navbar = document.querySelector(".navbar");
--- Script/script.js
+++ Script/script.js
@@ -58,6 +58,13 @@
 })();
 
 
- 
+  window.addEventListener("scroll", function () {
+      const navbar = document.querySelector(".navbar");
+      if (window.scrollY > 50) {
+          navbar.classList.add("scrolled");
+      } else {
+          navbar.classList.remove("scrolled");
+      }
+  });
 
 
